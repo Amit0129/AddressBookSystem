fd10a731cef089d84c8da7f85a9404c119fc4c98
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-30 14:26:54 +0530

    Adding Contacts to Address book

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        List<Contacts> addressList = new List<Contacts>();
+        public void AddContact(Contacts contacts)
+        {
+            addressList.Add(contacts);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine("Name : " + contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Zip : " + contact.Zip);
+                Console.WriteLine("PhoneNumber : " + contact.MobileNumber);
+                Console.WriteLine("Email : " + contact.Email);
+                Console.WriteLine("Contact is Added Successfully");
+            }
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,19 +6,44 @@
         {
             Console.WriteLine("Welcome to Address Book Program");
 
-            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Mobilenumber, Email");
+            Console.WriteLine("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Mobile Number: ");
+            long mobileNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter EMail: ");
+            string eMail = Console.ReadLine();
+
+            Console.WriteLine("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip: ");
+            int zip = Convert.ToInt32(Console.ReadLine()); 
 
             Contacts contacts = new Contacts()
             {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Console.ReadLine(),
-                MobileNumber = Console.ReadLine(),
-                Email = Console.ReadLine()
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                MobileNumber = mobileNumber,
+                Email = eMail
             };
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact(contacts);
+            addressBook.Display();
             
         }
     }
