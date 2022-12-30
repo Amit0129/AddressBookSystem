b6cc77775af0ad60cc6c066d46d05693c08c3ccc
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-30 15:08:39 +0530

    Edit an exiting contact by name

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,47 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
-
-            Console.WriteLine("Enter First Name: ");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("Enter Mobile Number: ");
-            long mobileNumber = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("Enter EMail: ");
-            string eMail = Console.ReadLine();
-
-            Console.WriteLine("Enter Address: ");
-            string address = Console.ReadLine();
-
-            Console.WriteLine("Enter City: ");
-            string city = Console.ReadLine();
-
-            Console.WriteLine("Enter State: ");
-            string state = Console.ReadLine();
-
-            Console.WriteLine("Enter Zip: ");
-            int zip = Convert.ToInt32(Console.ReadLine()); 
-
-            Contacts contacts = new Contacts()
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Address = address,
-                City = city,
-                State = state,
-                Zip = zip,
-                MobileNumber = mobileNumber,
-                Email = eMail
-            };
+            bool flag = true;
+            Contacts contact = new Contacts();
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact(contacts);
-            addressBook.Display();
-            
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
+                        contact = new Contacts()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            MobileNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Contact Information to Add, in form of FirstName, LastName, Addr, City, State, Zip, Mobile Number, Email.");
+                        contact = new Contacts()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            MobileNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        addressBook.AddContact(contact);
+                        addressBook.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the Contact Name to  Edit: ");
+                        string name = Console.ReadLine();
+                        addressBook.EditContact(name);
+                        Console.WriteLine("Contact is Edited Sucsessfully");
+                        addressBook.Display();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
+
         }
+
     }
+    
 }
