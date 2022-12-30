482e49ed31238b9b82620a3c15daf5fe30b8ad0e
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-30 14:13:43 +0530

    Creating a Contact Details

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Mobilenumber, Email");
+
+            Contacts contacts = new Contacts()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Console.ReadLine(),
+                MobileNumber = Console.ReadLine(),
+                Email = Console.ReadLine()
+            };
+            
         }
     }
 }
